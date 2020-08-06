9598c6474d6f616dccbf87a5e3db122157f4c904
Dari Alves <darieltondf@gmail.com>
2020-08-06 14:26:58 -0300

    day 2

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,10 @@
 import express from 'express';
+import cors from 'cors';
 import routes from './routes';
 
 const app = express();
 
+app.use(cors());
 // Faz a conversão do request body para um objeto do JS
 app.use(express.json());
 app.use(routes);
