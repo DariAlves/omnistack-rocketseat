ee6c643c8e19fc9ff94a7df340503273d3139279
Dari Alves <darieltondf@gmail.com>
2020-08-09 22:09:18 -0300

    day 3

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PageHeader from '../../components/PageHeader';
+import Input from '../../components/Input';
 
 import './style.css';
 
@@ -15,22 +16,25 @@ function TeacherForm() {
                 <fieldset>
                     <legend>Seus dados</legend>
 
-                    <div className="input-block">
-                        <label htmlFor="name">Nome completo</label>
-                        <input type="text" id="name" />
-                    </div>
+                    <Input name="name" label="Nome completo" />
+                    <Input name="avatar" label="Avatar" />
+                    <Input name="whatsapp" label="Whatsapp" />
+                    
+                </fieldset>
 
-                    <div className="input-block">
-                        <label htmlFor="avatar">Avatar</label>
-                        <input type="text" id="avatar" />
-                    </div>
+                <fieldset>
+                    <legend>Sobre a aula</legend>
 
-                    <div className="input-block">
-                        <label htmlFor="whatsapp">Whatsapp</label>
-                        <input type="text" id="whatsapp" />
-                    </div>
+                    <Input name="subject" label="Matéria" />
+                    <Input name="cost" label="Custo da sua hora por aula" />
                 </fieldset>
             </main>
+
+            <footer>
+                <p>
+                    <img src="" alt=""/>
+                </p>
+            </footer>
         </div>
     );
 }
